aec9a1d926dd1cfc7f0638fb6a5653842fc40cb8
zz <zhaosdjzu@gmail.com>
2022-07-15 20:34:10 +0800

    项目目录结构初始化

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,13 @@
 <template>
-  <div></div>
+  <div id="app">
+    <h1>黑马头条</h1>
+    <router-view />
+  </div>
 </template>
 
 <script>
 export default {
+  name: "App",
   data() {
     return {};
   },
